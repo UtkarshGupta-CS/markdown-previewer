f96fc8481a69ac134865825e3323624c2f43869e
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-06-08 12:28:46 +0530

    intiated webpack-dev-server

--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
 # MarkDown Previewer
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ npm start
+```
+
+#### For Any Query 
+
+[UtkarshGupta-CS](https://github.com/UtkarshGupta-CS)
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import { marked } from 'marked';
+import React from 'react';
+
+export default class App extends React.Component{
+   render(){
+      return (
+         <div>
+            <h1>Markdown Previewer</h1>
+         </div>
+      )
+   }
+}
